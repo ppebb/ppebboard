8086ca17b0d89cdd7e6eb4b7a705a7c21362be34
ppeb <ppeb@ppeb.me>
2025-07-12 12:36:32 -0500

    Don't launch if taking input from stdin

--- plugin/ppebboard.lua
+++ plugin/ppebboard.lua
@@ -7,7 +7,7 @@ vim.g.loaded_ppebboard = 1
 vim.api.nvim_create_autocmd("UIEnter", {
     group = vim.api.nvim_create_augroup("ppebboard", { clear = true }),
     callback = function()
-        if vim.fn.argc() == 0 then
+        if vim.fn.argc() == 0 and not vim.tbl_contains(vim.v.argv, "-") then
             require("ppebboard"):create()
         end
     end,
